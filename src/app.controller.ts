c36380be5ffde4f76ae24f70ec1eba84fb3f60b1
jacky.yyy <jacky.yyy@alipay.com>
2018-09-13 20:37:19 +0800

    初始化

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Get, Controller } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  root(): string {
+    return this.appService.root();
+  }
+
+  @Get('/test')
+  root1(): string {
+    return '333333';
+  }
+}
